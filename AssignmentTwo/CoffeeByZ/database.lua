8faf3cc1f36955a592ab7e2a14b6bb7bbbdbb9d9
Zoltan <zdebre@gmail.com>
2013-11-03 01:56:39 +0000

    item with transition

--- AssignmentTwo/CoffeeByZ/database.lua
+++ AssignmentTwo/CoffeeByZ/database.lua
@@ -2,21 +2,27 @@ local M = {}
 
 M = {
 	categories = {
-		{id= 1, name= 'Coffees'		    , image= 'images/coffees.jpg'	 },
-		{id= 2, name= 'Cakes'		 	, image= 'images/cakes.jpg'			},
-		{id= 3, name= 'Sandwiches'      , image= 'images/sandwiches.jpg' },
-		{id= 4, name= 'Salads'		    , image= 'images/salads.jpg' },
+		{id= 1, name= 'Coffee'		, image= 'product_images/coffees.jpg'	 			},
+		{id= 2, name= 'Cakes'		 	, image= 'product_images/cakes.jpg'					},
+		{id= 3, name= 'Sandwiches', image= 'product_images/sandwiches.jpg'		},
+		{id= 4, name= 'Salads'		, image= 'product_images/salads.jpg' 				}
 	},
 
 	products= {
-		{id=1, category_id= 1, name= 'Espresso'                 },
-		{id=2, category_id= 2, name= 'Carrot Cake'              },
-		{id=3, category_id= 3, name= 'BLT'                      },
-		{id=4, category_id= 4, name= 'Caeser Salad'             },
-		{id=5, category_id= 1, name= 'Cappucino'                },
-		{id=6, category_id= 2, name= 'Cheese Cake'              },
-		{id=7, category_id= 3, name= 'Ham and Cheese Sandwich'  },
-		{id=8, category_id= 4, name= 'Coleslow'                 }
+		{id=1, category_id= 1, name= 'Espresso'                , image='product_images/1.jpg', price='€ 1.50', details='Espresso is coffee brewed by forcing a small amount of nearly boiling water under pressure through finely ground coffee beans.' },
+		{id=2, category_id= 2, name= 'Carrot Cake'             , image='product_images/2.jpg', price='€ 3.50', details='Carrot cake is a cake or pie which contains carrots mixed into the batter. The carrot softens in the cooking process, and the cake usually has a soft, dense texture. The carrots themselves enhance the flavor, texture, and appearance of the cake.' },
+		{id=3, category_id= 3, name= 'BLT'                     , image='product_images/3.jpg', price='€ 4.30', details='A BLT is a type of bacon sandwich. The standard BLT is made up of five ingredients: bacon, lettuce, tomato, mayonnaise, and bread.' },
+		{id=4, category_id= 4, name= 'Caesar Salad'            , image='product_images/4.jpg', price='€ 5.10', details='A Caesar salad is a salad of romaine lettuce and croutons dressed with parmesan cheese, lemon juice, olive oil, egg, Worcestershire sauce, garlic, and black pepper. It is often prepared tableside.' },
+		{id=5, category_id= 1, name= 'Cappucino'               , image='product_images/5.jpg', price='€ 2.70', details='A cappuccino, is an Italian coffee drink which is traditionally prepared with espresso, hot milk, and steamed-milk foam. The name comes from the Capuchin friars, referring to the colour of their habits' },
+		{id=6, category_id= 2, name= 'Cheese Cake'             , image='product_images/6.jpg', price='€ 3.50', details='Cheesecake is a sweet dish consisting of two or more layers. The main, or thickest layer, consists of a mixture of soft, fresh cheese, eggs, and sugar.' },
+		{id=7, category_id= 3, name= 'Ham and Cheese Sandwich' , image='product_images/7.jpg', price='€ 4.50', details='The ham and cheese sandwich is a common type of sandwich. It is made by putting cheese and sliced ham between two slices of bread. The bread is sometimes buttered and toasted.' },
+		{id=8, category_id= 4, name= 'Coleslow'                , image='product_images/8.jpg', price='€ 3.90', details='Coleslaw, sometimes simply called slaw in some American dialects, is a salad consisting primarily of shredded raw cabbage, which is dressed with mayonnaise and/or buttermilk in some variations.' },
+		{id=9, category_id= 4, name= 'Garden Salad'            , image='product_images/9.jpg', price='€ 4.90', details='A garden salad (also green salad or tossed salad) is a salad consisting mostly of fresh vegetables. The base for the salad are greens such as lettuce or mesclun.' },
+		{id=10, category_id= 1, name= 'Caffee Latte'           , image='product_images/1.jpg', price='€ 4.90', details='A garden salad (also green salad or tossed salad) is a salad consisting mostly of fresh vegetables. The base for the salad are greens such as lettuce or mesclun.' },
+		{id=11, category_id= 1, name= 'Caffee Affogato'        , image='product_images/1.jpg', price='€ 4.90', details='A garden salad (also green salad or tossed salad) is a salad consisting mostly of fresh vegetables. The base for the salad are greens such as lettuce or mesclun.' },
+		{id=12, category_id= 1, name= 'Hot Chocolate'          , image='product_images/1.jpg', price='€ 4.90', details='A garden salad (also green salad or tossed salad) is a salad consisting mostly of fresh vegetables. The base for the salad are greens such as lettuce or mesclun.' },
+		{id=13, category_id= 1, name= 'Soya Caffee'            , image='product_images/1.jpg', price='€ 4.90', details='A garden salad (also green salad or tossed salad) is a salad consisting mostly of fresh vegetables. The base for the salad are greens such as lettuce or mesclun.' },
+		{id=14, category_id= 1, name= 'Americano'            	 , image='product_images/1.jpg', price='€ 4.90', details='A garden salad (also green salad or tossed salad) is a salad consisting mostly of fresh vegetables. The base for the salad are greens such as lettuce or mesclun.' },
 	}
 }
 
--- AssignmentTwo/CoffeeByZ/list.lua
+++ AssignmentTwo/CoffeeByZ/list.lua
@@ -13,12 +13,15 @@ local helper = require ( "helper" )
 -- Load database file
 local database = require ('database')
 
+local list, item, backButton
+
 -- Called when the scene's view does not exist:
 function scene:createScene( event )
 	local group = self.view
-	
+
 	helper.drawBackground(group)
-	
+
+	helper.drawMenuBar(group)
 
 end
 
@@ -31,85 +34,114 @@ function scene:enterScene( event )
 	local category_id = params.category_id
 
 	-- Listen for tableView events
-	-- Listener. Used to listen for TableView events, with the following events: 
-	-- event.limitReached - Indicates that the TableView has reached one of it's limits. 
+	-- Listener. Used to listen for TableView events, with the following events:
+	-- event.limitReached - Indicates that the TableView has reached one of it's limits.
 	-- event.direction - Returns the direction the TableView is moving in.
 	local function tableViewListener( event )
-        local phase = event.phase
-        local row = event.target
-        print( event.phase )
+    local phase = event.phase
+    local row = event.target
+    print( event.phase )
 	end
 
 	-- Handle row rendering
 	-- onRowRender (optional)
-	-- Listener. Used to listen for TableView row rendering events, with event types of: rowRender. 
-	-- This listener is initiated on both the initial rendering of the tableView rows and also when a row which was previously 
+	-- Listener. Used to listen for TableView row rendering events, with event types of: rowRender.
+	-- This listener is initiated on both the initial rendering of the tableView rows and also when a row which was previously
 	-- off-screen has moved back on screen. In your listener, event.row is a reference to the TableView row that was rendered.
 	local function onRowRender( event )
+
     local phase = event.phase
     local row = event.row
+		local rowImage = display.newImage(row, row.params.image )
+    rowImage:setReferencePoint(display.TopLeftReferencePoint)
+    rowImage.x = 0
+    rowImage.y = 0
+    rowImage.width, rowImage.height = 50, 50
+    
+    local rowTitle = display.newText(row, row.params.name, 0, 0, native.systemFontBold, 20 )
+    rowTitle:setReferencePoint(display.TopLeftReferencePoint)
+    rowTitle.x = 80
+    rowTitle.y = 10
+    rowTitle:setTextColor(0,0,0)
 
-    local rowTitle = display.newText( row, row.params.name, 0, 0, nil, 14 )
-    rowTitle.x = row.x - ( row.contentWidth * 0.5 ) + ( rowTitle.contentWidth * 0.5 )
-    rowTitle.y = row.contentHeight * 0.5
-    rowTitle:setTextColor( 0, 0, 0 )
+    local rowPrice = display.newText(row, row.params.price, 0,0, nil, 20)
+    rowPrice:setReferencePoint(display.TopLeftReferencePoint)
+    rowPrice.x = row.contentWidth - 50
+    rowPrice.y = row.contentHeight - 20
+    rowPrice:setTextColor(0,0,0)
+
 	end
 
 	-- Event for clicking on a product
 	-- Choosen product_id will be passed as parameter
-	local visitProduct = function (id)
-		local options = {
-			effect 	= 'slideLeft',
-			time 		= 200,
-			params 	= {
-								product_id = id
-								}
-		}
-		storyboard.gotoScene( 'item', options ) 
-	end
+
+
+
+
 
 	-- Handle touches on the row
-	-- Used to listen for TableView, with event phases of: 
-	-- tap, press, release, swipeLeft, swipeRight. 
+	-- Used to listen for TableView, with event phases of:
+	-- tap, press, release, swipeLeft, swipeRight.
 	-- In your listener, event.target is a reference to the TableView row that you interacted with.
 	local function onRowTouch( event )
-	    local phase = event.phase
-	    local row   = event.row
-	    print (phase)
-	    if "press" == phase then
-	    	print( "Touched row id:", row.params.id )
-	      visitProduct(row.params.id)
-	    end
+    local phase = event.phase
+    local row   = event.row
+    print (phase)
+
+    local onBackRelease = function ()
+      transition.to (list, {x = 0, time = 200, transition = easing.outExpo})
+      transition.to (backButton, {alpha = 0, time = 200, transition = easing.outQuad})
+    end
+
+    backButton = widget.newButton{width = 100, height = 50, label = 'Back', labelYOffset = -1, onRelease = onBackRelease}
+    backButton.alpha = 0
+    backButton.x = display.contentCenterX
+    backButton.y = display.contentHeight - backButton.contentHeight
+    group:insert(backButton)
+
+    if "press" == phase then
+      print( "Touched row id:", row.params.id )
+
+    elseif 'release' == phase then
+
+      transition.to (list, {x = - list.contentWidth, time = 200, transition = easing.outExpo})
+      --      transition.to (itemSelected.name, {x= display.contentCenterX, time = 200, transition = easing.outExpo})
+      transition.to (backButton, {alpha =1 , time = 200, transition = easing.outQuad})
+
+    end
+
 	end
 
 	-- Create a tableView
-	local tableView = widget.newTableView
+	list = widget.newTableView
 	{
-	    top = 100,
-	    width = 320, 
-	    height = 300,
-	    -- maskFile = "assets/mask-320x366.png",
-	    listener = tableViewListener,
-	    onRowRender = onRowRender,
-	    onRowTouch = onRowTouch,
+    top 					= 50,
+    width 				= 320,
+    height 				= (_H-150),
+    listener 			= tableViewListener,
+    onRowRender 	= onRowRender,
+    onRowTouch 		= onRowTouch,
+    maskFile      = 'images/mask-320x448.png',
 	}
-	group:insert( tableView )
+	group:insert( list )
 
-	local products = {}
 	local i, product
-	
+
 	-- get products for database where category_id is the choosen one
 	for i,product in pairs(database.products) do
 	  if product.category_id == category_id then
-	  	table.insert(products, product)
-	  	tableView:insertRow
-    	{ 
+	  	print(product.id)
+	  	list:insertRow
+    	{
     		rowHeight = 100,
     		rowColor = { 150, 160, 180, 200 },
-    		lineColor = { 0 },
-    		params = {       
-        		id 		= product.id,
-        		name 	= product.name
+    		lineColor = { 200 },
+    		params = {
+          id 			= product.id,
+          name 		= product.name,
+          price 	= product.price,
+          image 	= product.image,
+          details = product.details
       	}
     	}
 	  end
